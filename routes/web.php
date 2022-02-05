57ac08d14aafd9e2c35c7668dc76e52729b4bbad
Mohsin Shaikh <mohsinshaikh78601@gmail.com>
2022-02-05 16:29:06 +0530

    Home Page Added

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect()->route('auth.register');
-});
+    return view('welcome');
+})->name('welcome');
+
+// Route::get('/', function () {
+//     return redirect()->route('auth.register');
+// });
 
 Route::get('/admin/register', \App\Http\Livewire\Register::class)->name('auth.register');
